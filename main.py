cad0f7e6102882677bc93833284dd5a1332d2a39
moddeore <moddeore2006@gmail.com>
2025-05-10 17:15:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import pandas as pd
 import re
 
 import uvicorn
-
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=10000)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True) 
 
 
 
