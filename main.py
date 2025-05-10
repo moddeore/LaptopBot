e885024d70f5d5eaeebec95c14984f65ea9035f8
moddeore <moddeore2006@gmail.com>
2025-05-10 16:57:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import re
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
 
 
 
